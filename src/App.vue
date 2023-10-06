3aa6ec0f3151cd3506c1d7a0ad83651d2e411458
muteeburrehman <muteeb285@gmail.com>
2023-10-06 15:52:40 +0500

    Dynamic Components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,59 @@
 <template>
   <div>
     <!-- <TheHeader /> -->
-    <the-header></the-header>
+    <!-- <the-header></the-header>
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    ></user-info> -->
+    <!-- if we have to send only to default slot -->
+    <!-- <course-goals #default="slotProps">
+      <h2>
+        {{ sloProps.item }}
+      </h2>
+      <p>
+        {{ sloProps['another-prop'] }}
+      </p>
+    </course-goals> -->
+    <!-- <course-goals>
+     <template #default="sloProps">
+      <h2>
+        {{ sloProps.item }}
+      </h2>
+      <p>
+        {{ sloProps['another-prop'] }}
+      </p>
+     </template>
+    </course-goals> -->
+
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <manage-goals v-if="selectedComponent=== 'manage-goals'"></manage-goals>
+    <active-goals v-if="selectedComponent=== 'active-goals'"></active-goals> -->
+    <!-- Now we will get the same result but this time using dynamic components -->
+    <!-- This compoonent does not work key alone. and require key that is ':is' -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue'
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 export default {
   components:{
     'the-header': TheHeader,
-    'badge-list': BadgeList,
-    'user-info': UserInfo
+    // 'badge-list': BadgeList,
+    // 'user-info': UserInfo,
+    // 'course-goals': CourseGoals,
+    'active-goals': ActiveGoals,
+    'manage-goals': ManageGoals
     //We can also define components locally in this way
     // TheHeader: TheHeader
     //Also write in this way
@@ -27,6 +61,7 @@ export default {
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -34,6 +69,11 @@ export default {
       },
     };
   },
+  methods:{
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
